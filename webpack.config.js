cf3e7e00766e0f3ce88853107d0ebc5646f32daa
Gerrit Degenhardt <gerrit@degenhardt.be>
2020-08-21 15:40:47 +0200

    implemented npm

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// const path = require('path');
+
+module.exports = {
+    node: {
+        __dirname: true
+    },
+    entry: './src/js/index.js',
+    output: {
+        path: __dirname + '/dist',
+        // path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Adds CSS to the DOM by injecting a `<style>` tag
+                        loader: 'style-loader'
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader'
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('./node_modules/autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    }
+};
